8a6d9ff68d741fdd96f6afe464d9914fcc46919d
sandyy221 <kurniawansandy808.com>
2024-10-03 10:31:33 +0700

    Percobaan1

--- Pemilihan2Percobaan127.java
+++ Pemilihan2Percobaan127.java
@@ -6,15 +6,12 @@ public class Pemilihan2Percobaan127 {
         int tahun;
         System.out.print("Masukan tahun: ");
         tahun = input27.nextInt();
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) !=0);
-            System.out.println("Tahun kabisat");
-            
-        }   else {
-                System.out.println("Bukan tahun kabisat");
-             }
         
-        
-        
+        if ((tahun % 4) != 0) {
+            if ((tahun % 100) != 0);
+            System.out.println("Tahun kabiset");
+        }   else
+            System.out.println("Bukan tahun kabiset");
+
     }
 }
